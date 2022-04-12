1defdfbae80cb91ae5bc5200c9c6210c298fe129
Austin Vaughan <99289671+TemporaryPlaceholderAccount@users.noreply.github.com>
2022-04-11 23:09:05 -0500

    Add files via upload

---
+++ Exercise8.cs
@@ -0,0 +1,25 @@
+﻿namespace MyFirstApplication;
+
+public class Exercise8
+{
+    const int Foot = 12;//constant
+
+    static void Ruler(int feet)
+    {
+        Console.WriteLine(Foot * feet);//multiplication
+    }
+
+    static void Area(int length, int width)//8-2
+    {
+        Console.WriteLine(width * length);
+    }
+
+   static public void Main()
+    {
+        Ruler();
+        //tested with three
+        Area();
+        //tested with 5 & 6 for 30
+    }
+
+} 
