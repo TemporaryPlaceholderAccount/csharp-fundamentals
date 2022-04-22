21d33e61b4694ee195b103cc384d3a28ca0868f6
Austin Vaughan <99289671+TemporaryPlaceholderAccount@users.noreply.github.com>
2022-04-21 20:19:44 -0500

    Add files via upload

---
+++ Boat.cs
@@ -0,0 +1,58 @@
+﻿namespace MyFirstApplication;
+
+public class Boat
+{
+    private string _color;
+    private string _materials;
+    private int _size;
+
+    //Expression Body Definition Properties
+    public string Boat_hullcolor
+    {
+        get => _color;
+        set => _color = value;
+    }
+
+    public string Boat_materials
+    {
+        get => _materials;
+        set => _materials = value;
+    }
+
+    public int Boat_size
+    {
+        get => _size;
+        set => _size = value;
+    }
+
+    //constructor 1
+    public Boat(string Color, string materials, int size)
+    {
+        Boat_hullcolor = Color;
+        Boat_materials = materials;
+        Boat_size = size;
+    }
+
+    //constructor 2
+    public Boat(string Boat_hullcolor, string Boat_materials)
+        : this(Boat_hullcolor, Boat_materials, 10) { }
+
+    //c3
+    public Boat()
+        : this("Black", "Wooden") { }
+
+    //method
+    public static void Boat_method()
+    {
+        Console.WriteLine("The Boat sails the seas");
+    }
+}
+
+class Program
+{
+
+    public static void Main()
+    {
+        Boat.Boat_method();
+    }
+}
---
+++ Horse.cs
@@ -0,0 +1,41 @@
+﻿namespace MyFirstApplication;
+
+public class Horse
+{
+
+    //auto props?
+    public string Horse_haircolor { get; set;}
+    public string Horse_gender { get; set;}
+    public int Horse_age { get; set;}
+
+    //constructor 1
+    public Horse(string Hair, string Gender, int Age)
+    {
+        Horse_haircolor = Hair;
+        Horse_gender = Gender;
+        Horse_age = Age;
+    }
+
+    //constructor 2
+    public Horse(string Horse_haircolor, string Horse_gender)
+        : this(Horse_haircolor, Horse_gender, 10) { }
+
+    //c3
+    public Horse()
+        : this("Black", "Male") { }
+
+    //method
+    public static void Horse_method()
+    {
+        Console.WriteLine("The horse gallops into the sunset");
+    }
+}
+
+class Program
+{
+
+    public static void Main()
+    {
+        Horse.Horse_method();
+    }
+}
