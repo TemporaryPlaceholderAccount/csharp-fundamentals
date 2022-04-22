864971f5312334e2f339318d96756cf2f61a4c65
Austin Vaughan <99289671+TemporaryPlaceholderAccount@users.noreply.github.com>
2022-04-22 16:33:03 -0500

    Add files via upload

---
+++ Exercise17.cs
@@ -0,0 +1,40 @@
+﻿namespace MyFirstApplication;
+
+
+public class ExerciseTemplate
+{
+
+    static public void Catch(int one, int two)
+    {
+        try
+        {
+            Console.WriteLine(one / two);
+        }
+
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine($"{ex.Message} Can't do that.");
+        }
+
+
+    }
+
+
+    static public void Farm(string Horse)
+    {
+        if (Horse == null)
+        {
+            throw new NullReferenceException("The horse is null");
+        }
+        else 
+        { 
+        Console.WriteLine("The horse isn't null");
+        }
+    }
+    static public void Main()
+    {
+        Catch(1, 0);
+        Farm("Horse");
+    }
+
+}
