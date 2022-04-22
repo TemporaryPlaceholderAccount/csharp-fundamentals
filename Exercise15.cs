e98188a6ceedfb0ec28eb9fcb46ff6d51d14dc09
Austin Vaughan <99289671+TemporaryPlaceholderAccount@users.noreply.github.com>
2022-04-22 14:42:59 -0500

    Add files via upload

---
+++ Exercise15.cs
@@ -0,0 +1,37 @@
+﻿namespace MyFirstApplication;
+
+public enum FavMov { Pirates, Cabin, Endgame };
+
+public class enumerations//15, enums & tuples
+{
+        static public String Faves(FavMov movies)//enum  
+        {
+        string message = movies switch
+        {
+            FavMov.Pirates => $"My favorite movie is {FavMov.Pirates}",
+            FavMov.Cabin => $"My favorite movie is {FavMov.Cabin}",
+            FavMov.Endgame => $"My favorite movie is {FavMov.Endgame}",//the switch
+            _ => "I've never heard of that one."
+        };
+            return message;
+        }
+
+   public Tuple TupleEx(int num, string movie)//i cant get the tuple return type to work the way you do
+    {
+        (int, string) example = (2, "Cabin");
+
+        Console.WriteLine(example);
+
+        int value = (int)Enum.Value;//why? the value is right there
+
+        string movies = Enum.Value.ToString();//i give. uncle.
+
+        Console.WriteLine(example);
+    }
+
+    static public void Main()
+    {
+        Faves(FavMov.Cabin);
+    }
+
+}
